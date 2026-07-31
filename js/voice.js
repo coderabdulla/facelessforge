80127da35d70ba8e0a2d1935bf31a473862e1bf4
coderabdulla <144826383+coderabdulla@users.noreply.github.com>
2026-07-31 14:42:43 +0530

    Update voice.js

--- js/voice.js
+++ js/voice.js
@@ -1,1 +1,42 @@
+"use strict";
+export class VoiceEngine {
 
+    constructor() {
+        this.rate = 1;
+    }
+
+    setRate(rate) {
+        this.rate = rate;
+    }
+
+    async generateVoice(text) {
+
+        const response = await fetch("/api/generate-voice", {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                text,
+                rate: this.rate
+            })
+
+        });
+
+        if (!response.ok) {
+            throw new Error("Voice generation failed.");
+        }
+
+        return await response.blob();
+
+    }
+
+}
+setRate(rate){
+
+this.rate = rate;
+
+}
